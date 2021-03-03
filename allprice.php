6133e578e75c7919205c198b6c0b65cb615a256b
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2021-03-04 01:03:47 +0545

    Update allprice.php

--- allprice.php
+++ allprice.php
@@ -20,18 +20,17 @@ $error = error_get_last();
 
 foreach($html->find('tr') as $data) {
     $item['title']   = $data->find('td', 0)->plaintext;
-     if($item['title']!=null){
-        $item['tradecompany']    = $data->find('td[2]', 0)->plaintext;
-         $item['nooftransaction'] = $data->find('td[3]', 0)->plaintext;
-         $item['maxprice'] = $data->find('td[4]', 0)->plaintext;
-         $item['minprice'] = $data->find('td[5]', 0)->plaintext;
-         $item['closingprice'] = $data->find('td[6]', 0)->plaintext;
-         $item['tradedshares'] = $data->find('td[7]', 0)->plaintext;
-         $item['amount'] = $data->find('td[8]', 0)->plaintext;
-         $item['previousclosing'] = $data->find('td[9]', 0)->plaintext;
-         $item['difference'] = $data->find('td[10]', 0)->plaintext;
-         $alldata[] = $item;
-     }    
+    $item['tradecompany']    = $data->find('td[2]', 0)->plaintext;
+    $item['nooftransaction'] = $data->find('td[3]', 0)->plaintext;
+    $item['maxprice'] = $data->find('td[4]', 0)->plaintext;
+    $item['minprice'] = $data->find('td[5]', 0)->plaintext;
+    $item['closingprice'] = $data->find('td[6]', 0)->plaintext;
+    $item['tradedshares'] = $data->find('td[7]', 0)->plaintext;
+    $item['amount'] = $data->find('td[8]', 0)->plaintext;
+    $item['previousclosing'] = $data->find('td[9]', 0)->plaintext;
+    $item['difference'] = $data->find('td[10]', 0)->plaintext;
+    $alldata[] = $item;
+}
 }
 
 echo json_encode($alldata);
